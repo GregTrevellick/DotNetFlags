28e67dc1e5e427d0cd311180d927742028a0c5e1
Greg Trevellick <gtrevellick@googlemail.com>
2020-03-21 20:33:42 +0000

    .

---
+++ Src/DotNetFlags/DummyCodeForSonar.cs
@@ -0,0 +1,8 @@
+public class Dummy
+{
+	// a dummy class so that sonar doesn't report false-positive results due to this repo not actually having any code
+	public void Dummy()
+	{
+		throw new NotImplemetedException();
+	}
+}
