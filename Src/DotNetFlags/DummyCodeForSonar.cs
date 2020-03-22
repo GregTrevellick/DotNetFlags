dbee32c1009e2b07147b56107bba741b920df4c6
Greg Trevellick <gtrevellick@googlemail.com>
2020-03-22 16:08:01 +0000

    added mstest2

--- Src/DotNetFlags/DummyCodeForSonar.cs
+++
@@ -1,9 +0,0 @@
-using System;
-public class Dummy
-{
-	// a dummy class so that sonar doesn't report false-positive results due to this repo not actually having any code
-	public Dummy()
-	{
-		throw new NotImplementedException();
-	}
-}
